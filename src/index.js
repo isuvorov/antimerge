af91bbfea598c840de75c345d8b9ce8e207dfeb4
nata <natavts@gmail.com>
2017-10-02 11:53:04 +0300

    new tests + lint fixes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import _ from 'lodash';
 
 const antimerge = (a, b) => {
   if (!_.isObject(a)) return null;
-	if (!_.isObject(b)) return null;
-	const res = {};
-	for (const k in a) {
-		if( a.hasOwnProperty( k ) ) {
-			if (_.isEqual(a[k], b[k])) continue;
-			res[k] = a[k]
-		}
-	}
+  if (!_.isObject(b)) return null;
+  const res = {};
+  for (const k in a) {
+    if ({}.hasOwnProperty.call(a, k)) {
+      if (!_.isEqual(a[k], b[k])) {
+        res[k] = a[k];
+      }
+    }
+  }
   const diff = _.differenceWith(Object.keys(b), Object.keys(a), _.isEqual);
-	diff.forEach((d) => {
-		res[d] = undefined;
-	})
-	return res;
-}
+  diff.forEach((d) => {
+    res[d] = undefined;
+  });
+  return res;
+};
 
 export default antimerge;
--- tests/antimerge.spec.js
+++ tests/antimerge.spec.js
@@ -1,10 +1,47 @@
-import antimerge from '../src/index.js';
-import testcases from './testcases/antimerge.js';
+/* eslint-env jest */
+import _ from 'lodash';
+import deepmerge from 'deepmerge';
+import antimerge from '../src/index';
 
-describe('antimerge testes', () => {
+describe('antimerge set', () => {
+  describe('antimerge testes with manual result', () => {
+    const testcases = require('./testcases/antimerge').default;
     testcases.forEach((test) => {
-         it(`should return ${JSON.stringify(test[2])}`, () => {
-             expect(antimerge(test[0], test[1])).toEqual(test[2]);
-         })
-    })
+      it(`should return ${JSON.stringify(test[2])}`, () => {
+        expect(antimerge(test[0], test[1])).toEqual(test[2]);
+      });
+    });
+  });
+
+  describe('antimerge testes with Object.assign', () => {
+    const testcases = require('./testcases/antimerge').default;
+    testcases.forEach((test) => {
+      it(`should return ${JSON.stringify(test[2])}`, () => {
+        const result = antimerge(test[0], test[1]);
+        if (result) {
+          const newObject = Object.assign(test[1], result);
+          expect(_.isEqual(test[0], JSON.parse(JSON.stringify(newObject)))).toEqual(true);
+        } else {
+          expect(result).toEqual(test[2]);
+          expect(result).toEqual(null);
+        }
+      });
+    });
+  });
+
+  describe('antimerge testes with deepmerge', () => {
+    const testcases = require('./testcases/antimerge').default;
+    testcases.forEach((test) => {
+      it(`should return ${JSON.stringify(test[2])}`, () => {
+        const result = antimerge(test[0], test[1]);
+        if (result) {
+          const newObject = deepmerge(test[1], result);
+          expect(_.isEqual(test[0], JSON.parse(JSON.stringify(newObject)))).toEqual(true);
+        } else {
+          expect(result).toEqual(test[2]);
+          expect(result).toEqual(null);
+        }
+      });
+    });
+  });
 });

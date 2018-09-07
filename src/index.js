844ff0af9b904896b1c63c3119f5d6f11d9ccf40
Igor Suvorov <hi@isuvorov.ru>
2018-09-07 18:32:55 +0300

    antimergeDeep

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import isObject from 'lodash/isObject';
-import isEqual from 'lodash/isEqual';
-import differenceWith from 'lodash/differenceWith';
+import antimerge from './antimerge';
+import antimergeDeep from './antimergeDeep';
 
-const antimerge = (a, b) => {
-  if (!isObject(a)) return null;
-  if (!isObject(b)) return null;
-  const res = {};
-  for (const k in a) {
-    if (Object.prototype.hasOwnProperty.call(a, k)) {
-      if (isEqual(a[k], b[k])) continue;  // eslint-disable-line no-continue
-      res[k] = a[k];
-    }
-  }
-  const diff = differenceWith(Object.keys(b), Object.keys(a), isEqual);
-  diff.forEach((d) => {
-    res[d] = undefined;
-  });
-  return res;
-};
-
+export { antimergeDeep, antimerge };
 export default antimerge;
